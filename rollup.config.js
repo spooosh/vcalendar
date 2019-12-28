7582b6dcd6437ef74fd2118b6c5940b9030c320a
spooosh <spooosh13@gamail.com>
2019-12-28 20:16:47 +0300

    update rollup options build package

--- rollup.config.js
+++ rollup.config.js
@@ -4,45 +4,57 @@ import vue from 'rollup-plugin-vue';
 
 import babel from 'rollup-plugin-babel';
 
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import buble from '@rollup/plugin-buble';
 
 import {terser} from 'rollup-plugin-terser';
 
+import replace from 'rollup-plugin-replace';
+import minimist from 'minimist';
 
-export default {
+const argv = minimist(process.argv.slice(2));
+
+const config = {
     input: 'src/index.js', // Путь до относительного package.json
     output: {
         name: 'index',
-        dir: './dist/',
-        exports: 'named',
-        format: 'esm'
+        exports: 'named'
     },
     plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
         scss({
             prefix: '@import "./src/shared/_variables.scss";'
         }),
-        vue({
-            css: true,
-            styleToImports: true,
-            compileTemplate: true
+        resolve({
+            mainFields: ['jsnext:main', 'main'],
+            browser: true
         }),
         babel({
             exclude: 'node_modules/**',
             runtimeHelpers: true
         }),
-        resolve({
-            jsnext: true,
-            main: true,
-            browser: true
-        }),
         commonjs({
             experimentalDynamicImport: true
         }),
+        vue({
+            css: true,
+            styleToImports: true,
+            compileTemplate: true,
+            template: {
+                isProduction: true
+            }
+        }),
         buble({
             transform: {modules: true}
-        }),
-        terser()
+        })
     ]
 };
+
+if (argv.format === 'iife') {
+    config.plugins.push(terser());
+}
+
+export default config;
